084d48e4a1a5637191e38c308a8fcff8612b61b5
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-10-04 00:21:06 +0700

    dev

--- app/models.py
+++ app/models.py
@@ -1,4 +1,12 @@
 from dataclasses import dataclass
 
 
-# add dataclass here
+@dataclass
+class Actor:
+    id: int
+    first_name: str
+    last_name: str
+
+
+if __name__ == "__main__":
+    pass
